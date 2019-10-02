1587e54246f1257247ffb7f8f4b7446e5fe074ab
BuildTools <unconfigured@null.spigotmc.org>
2019-10-02 12:47:10 +0800

    add

--- main.ts
+++ main.ts
@@ -1,1 +1,15 @@
 
+/**
+ * Writing data for hacking stem experiments
+ */
+//% weight=94 color=#EC7505 icon="\uf1b3"
+namespace STEM {
+    /**
+     * Checks if the goal is reached by looking for the Gold Block direction down to agent
+     */
+    //% blockId=send_message block="send message"
+    //% weight=30
+    export function sendMessage(message: string) {
+        return;
+    }
+}
