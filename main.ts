00e8e208dc68c21b49a5112b5bb0a9dc8e6f4162
BuildTools <unconfigured@null.spigotmc.org>
2019-10-03 09:16:37 +0800

    add

--- main.ts
+++ main.ts
@@ -9,6 +9,6 @@ namespace STEM {
     //% blockId=send_message block="send message %message"
     //% weight=30
     export function sendMessage(message: string) {
-        // BrowserUtils.loadAjaxAsync("http://www.baidu.com");
+        let resp = net.request("GET", "http://www.baidu.com");
     }
 }
